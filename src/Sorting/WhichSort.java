2c661fa7d1a7514b4a1a182544be8117b8dc0a2d
Brandon <itzbrandonlee@yahoo.com>
2023-04-27 22:29:46 -0700

    sorting complete

---
+++ src/Sorting/WhichSort.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+public class WhichSort {
+
+    /* Series of questions to find out which sort would be the easiest to use for each one:
+
+    #1 Sort 10 schools around your house by distance
+        insertion sort
+
+    #2 eBay sorts listings by the current bid amount
+        radix or counting sort - bids rae usually within a certain number range
+
+    #3 sport scores on espn
+        Quick sort
+
+    #4 Massive database (cant fit all to memory) needs to sort through the past years user data
+        Merge sort
+
+    #5 almost sorted udemy review data needs to update and add two new reviews
+        insertion sort
+
+    #6 temperature records for the past 50 years in canada
+        quick sort
+
+    #7 large username database needs to be sorted. Data is very random
+        Merge sort if not worried about memory
+        quick sort if size is not an issue
+
+    #8 you want to teach sorting for the first time
+        Bubble sort/ selection sort
+
+     */
+}
